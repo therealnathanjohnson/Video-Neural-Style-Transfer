53b03a39dc298ad1f57b739a685bc678b2fb446a
therealnathanjohnson <therealnathanjohnson7@gmail.com>
2026-02-24 21:15:14 -0500

    Update keyframe_selection_utils.py

--- keyframe_selection_utils.py
+++ keyframe_selection_utils.py
@@ -108,7 +108,7 @@ if __name__ == "__main__":
                         help="Don't display images")
 
     #list chosen keyframe numbers. `nargs='*'` so we can accept an empty list
-    parser.add_argument("--chosen_keyframes", type=int, nargs="*",
+    parser.add_argument("--chosen_keyframes", type=int, nargs="*", default=[],
                     help="List of keyframe indices (e.g. --frames 0 19 46 84)")
     
     args = parser.parse_args()
